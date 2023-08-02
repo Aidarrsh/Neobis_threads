3a66cbb63c8808bb926a3bfa34ef7d7ae5b70d65
aidar <sharipov_ai@auca.kg>
2023-08-02 15:38:04 +0600

    Initial Commit

---
+++ Neobis_threads/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_threads
+//
+//  Created by Айдар Шарипов on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
