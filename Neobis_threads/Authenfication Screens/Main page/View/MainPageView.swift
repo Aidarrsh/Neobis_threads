8736d4505d01e20417ce46c48fed629a7680d078
aidar <sharipov_ai@auca.kg>
2023-08-15 15:31:19 +0600

    T2-136 For you and Following sections are finished

--- Neobis_threads/Authenfication Screens/Main page/View/MainPageView.swift
+++ Neobis_threads/Authenfication Screens/Main page/View/MainPageView.swift
@@ -10,6 +10,46 @@ import UIKit
 import SnapKit
 
 class MainPageView: UIView {
+    
+    private lazy var threadIcon: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "ThreadIcon")
+        
+        return image
+    }()
+    
+    lazy var forYouSection: UIButton = {
+        let button = UIButton()
+        button.setTitle("For you", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.sfSemiBold(ofSize: 17)
+        
+        return button
+    }()
+    
+    lazy var leftSectionBottomLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        
+        return view
+    }()
+    
+    lazy var followingSection: UIButton = {
+        let button = UIButton()
+        button.setTitle("Following", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.sfSemiBold(ofSize: 17)
+        
+        return button
+    }()
+    
+    lazy var rightSectionBottomLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -26,10 +66,47 @@ class MainPageView: UIView {
     }
     
     func setupViews() {
-        
+        addSubview(threadIcon)
+        addSubview(forYouSection)
+        addSubview(leftSectionBottomLine)
+        addSubview(followingSection)
+        addSubview(rightSectionBottomLine)
     }
     
     func setupConstraints() {
+        threadIcon.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(flexibleHeight(to: 62))
+            make.leading.equalToSuperview().inset(flexibleWidth(to: 181.37))
+            make.trailing.equalToSuperview().inset(flexibleWidth(to: 181.52))
+            make.bottom.equalToSuperview().inset(flexibleHeight(to: 755))
+        }
         
+        forYouSection.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(flexibleHeight(to: 105))
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview().inset(flexibleWidth(to: 196))
+            make.bottom.equalToSuperview().inset(flexibleHeight(to: 699))
+        }
+        
+        leftSectionBottomLine.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(flexibleHeight(to: 151.5))
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview().inset(flexibleWidth(to: 196))
+            make.bottom.equalToSuperview().inset(flexibleHeight(to: 699))
+        }
+        
+        followingSection.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(flexibleHeight(to: 105))
+            make.leading.equalToSuperview().inset(flexibleWidth(to: 197))
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(flexibleHeight(to: 699))
+        }
+        
+        rightSectionBottomLine.snp.makeConstraints{ make in
+            make.top.equalToSuperview().inset(flexibleHeight(to: 152))
+            make.leading.equalToSuperview().inset(flexibleWidth(to: 197))
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(flexibleHeight(to: 699))
+        }
     }
 }
